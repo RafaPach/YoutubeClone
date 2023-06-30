33269615a8443b5a3f6a59bf5c212d5e9a9f5952
RafaPach <rafael_eli_pacheco@hotmail.com>
2023-06-30 15:44:49 +0100

    added components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+import SearchBar from './SearchBar';
+
+import { logo } from '../utils/constants.js';
+
+const Navbar = () => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      position: 'sticky',
+      background: 'black',
+      top: 0,
+      justifyContent: 'space-between',
+    }}
+  >
+    <Link to="/home" style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={logo} alt="logo" height={45}></img>
+    </Link>
+    <SearchBar />
+  </Stack>
+);
+
+export default Navbar;
